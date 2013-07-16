82652f479ba964d361895495c32eaea2ce3e92d7
oles89 <apoltarjonoks@qmail.com>
2013-07-16 14:30:59 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConnectToFrend::Application.config.secret_key_base = 'bab5f0441cd0f44f8330c07dd507426d4308f3afb44d7f2fff04aede8ee7e5c0d818d8039e42bacfffa43fcff2f369950b1e1389a6c6360a45d14afe7b171752'
